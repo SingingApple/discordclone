516bcb83a6ab98364f5b02eac6e8b0cd35ed97e5
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-10-11 02:14:22 +0530

    Sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import './Sidebar.css'
+import React from "react";
+import "./Sidebar.css";
+import AddIcon from "@material-ui/icons/Add";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <h2>This is sidebar</h2>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Skibi Programmer</h3>
+        <ExpandMoreIcon></ExpandMoreIcon>
+      </div>
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon></ExpandMoreIcon>
+            <h4>Text Channels</h4>
+          </div>
+          <AddIcon className="sidebar__addChannel"></AddIcon>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
