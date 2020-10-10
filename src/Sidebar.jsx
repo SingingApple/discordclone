a501a3b8fd021d5636c2020bd1394e5dceaea9cc
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-10-10 23:51:00 +0530

    File structure

---
+++ src/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Sidebar.css'
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h2>This is sidebar</h2>
+        </div>
+    )
+}
+
+export default Sidebar
